d0ede53cd2a924a786bce7d60702ead0ff0651d4
bkvikram007 <167081150+bkvikram007@users.noreply.github.com>
2024-11-21 17:46:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express'); //Import the express dependency
-const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
+const app = require('express')();
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
+const PORT =8080;
 
-app.get('/pass.html', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('pass.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+app.listen(
+    PORT,
+    () => console.log(`server running on ${PORT}`)
+);
+
+app.get('/fruits', (req, res)=> {
+    res.send({
+        apple: 2,
+        banana: 12
+    })
 });
-app.get('/signup.html', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('signup.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
-app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
+app.get('/veggies', (req, res)=> {
+    res.send({
+        potato: 2,
+        brinjal: 12        
+    })
 });
